8eb39b43151624c8024b86d1cbc4b0892eb48fe0
Oleg Utkin <oleg_utkin@me.com>
2017-02-18 16:04:57 +0300

    Добавлен класс Primes

---
+++ lab1/Primes.java
@@ -0,0 +1,20 @@
+// Вывод всех простых чисел до 100
+public class Primes {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 100; ++i)
+            if (isPrime(i))
+                System.out.println(i);
+
+    }
+
+    // Определение простого числа
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; ++i)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    
+}
