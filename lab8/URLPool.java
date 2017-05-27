3914648a794997612913d0edaa9d2b85cdbc067f
Oleg Utkin <oleg_utkin@me.com>
2017-05-28 00:00:51 +0300

    Добавлено описание функций

--- lab8/URLPool.java
+++ lab8/URLPool.java
@@ -5,6 +5,7 @@ class URLPool {
     private LinkedList<URLDepthPair> scannedUrls = new LinkedList<URLDepthPair>();
     private LinkedList<URLDepthPair> urlsToScan = new LinkedList<URLDepthPair>();
 
+    /* Добавление ссылки в список для сканирования */
     public synchronized boolean addToScan(URLDepthPair urlDepthPair) {
         if (scannedUrls.contains(urlDepthPair))
             return false;
@@ -12,6 +13,7 @@ class URLPool {
             return urlsToScan.add(urlDepthPair);
     }
 
+    /* Добавление ссылки в список отсканированных ссылок */
     public synchronized boolean addScanned(URLDepthPair urlDepthPair) {
         if (scannedUrls.contains(urlDepthPair))
             return false;
@@ -19,6 +21,7 @@ class URLPool {
             return scannedUrls.add(urlDepthPair);
     }
 
+    /* Получение следующей ссылки для сканирования и удаление её из списка */
     public synchronized URLDepthPair get() {
         return urlsToScan.pollFirst();
     }
